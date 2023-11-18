f66ebdf48824f39b1737b029aad70b3f3f5982d3
Th3-attacker <haje.malik5@gmail.com>
2023-11-18 16:55:15 +0000

    responsive design

--- inedx.js
+++ inedx.js
@@ -1,11 +1,15 @@
 const toggleBtn = document.getElementById("toggleBtn");
 const closeBtn = document.getElementById("closeBtn");
+// const opacityBody = document.querySelector('body');
 const responsiveMenu = document.getElementById("responsiveMenu");
 
 toggleBtn.addEventListener("click", function () {
   responsiveMenu.classList.toggle("hidden");
+  // opacityBody.style.backgroundColor = 'rgba(0,0,0, 0.7)';
+
 });
 
 closeBtn.addEventListener("click", function () {
   responsiveMenu.classList.add("hidden");
+  // opacityBody.style.backgroundColor = '#fff';
 });
